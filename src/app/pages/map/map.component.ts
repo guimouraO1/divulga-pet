6e9a31730c6cf4ac584d2219087d957463a74d51
guimouraO1 <guimoura.oliveira@gmail.com>
2024-04-26 16:28:10 -0300

    add marker click function

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import L, { icon, latLng, LatLngBounds,  marker, Marker, tileLayer } from 'leaflet';
+import L, {
+  icon,
+  latLng,
+  LatLngBounds,
+  marker,
+  Marker,
+  tileLayer,
+} from 'leaflet';
 import { lastValueFrom, Subject, takeUntil } from 'rxjs';
 import { PetService } from '../../services/pet.service';
 import { Pet } from '../../models/pet.model';
@@ -11,9 +18,8 @@ import { ActivatedRoute, Router } from '@angular/router';
   standalone: true,
   imports: [LeafletModule],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrl: './map.component.scss',
 })
-
 export class MapComponent implements OnInit, OnDestroy {
   map: any;
   layers: any[] = [];
@@ -21,51 +27,55 @@ export class MapComponent implements OnInit, OnDestroy {
 
   options = {
     layers: [
-      tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', { maxZoom: 18 })
+      tileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        { maxZoom: 18 }
+      ),
     ],
     zoom: 4,
     center: latLng(-20, -65),
     minZoom: 4,
     maxBounds: new LatLngBounds(
       latLng(-40.0, -110.0), // Latitude mínima e longitude mínima (Sul e Oeste)
-      latLng(10.0, -10.0)    // Latitude máxima e longitude máxima (Norte e Leste)
-    )    
+      latLng(10.0, -10.0) // Latitude máxima e longitude máxima (Norte e Leste)
+    ),
   };
 
-  constructor(private petService: PetService, private route: ActivatedRoute){
-    
-  }
-  
+  constructor(private petService: PetService, private route: ActivatedRoute) {}
+
   async ngOnInit() {
     this.route.queryParamMap
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(async (params) => {
-      const lat: any = params.get('lat');
-      const lon: any = params.get('lon');
-      const zoom: any = params.get('zoom');
-      if(zoom){
-        this.options.zoom = parseInt(zoom);
-      }
-      if(lat && lon){
-        this.options.center = latLng(parseFloat(lat), parseFloat(lon));
-      }
-    });
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(async (params) => {
+        const lat: any = params.get('lat');
+        const lon: any = params.get('lon');
+        const zoom: any = params.get('zoom');
+        if (zoom) {
+          this.options.zoom = parseInt(zoom);
+        }
+        if (lat && lon) {
+          this.options.center = latLng(parseFloat(lat), parseFloat(lon));
+        }
+      });
 
-    await this.getPublications();
+    // await this.getPublications();
   }
-
 
   async getPublications() {
     try {
-      const res: any = await lastValueFrom(this.petService.getPublications('', 0, 100, 'find'));
+      const res: any = await lastValueFrom(
+        this.petService.getPublications('', 0, 100, 'find')
+      );
 
       res.publications.forEach((pet: Pet) => {
-        const marker = L.marker(JSON.parse(pet.latlon), { icon: this.createIcon(pet.filename) });
-        this.layers.push(marker); 
+        const marker = L.marker(JSON.parse(pet.latlon), {
+          icon: this.createIcon(pet.filename),
+        }).on('click', () => {
+          console.log(pet.name);
+        });
+        this.layers.push(marker);
       });
-    } catch (error) {
-
-    }
+    } catch (error) {}
   }
 
   createIcon(filename: string) {
@@ -76,8 +86,9 @@ export class MapComponent implements OnInit, OnDestroy {
     });
   }
 
-  onMapReady(map: any) {
+  async onMapReady(map: any) {
     this.map = map;
+    await this.getPublications();
   }
 
   ngOnDestroy() {
